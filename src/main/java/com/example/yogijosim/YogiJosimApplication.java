08cd415c7ddfdda7050b0876e0b349038eda5bfe
jae woo Jung <awfjol2008@gmail.com>
2025-08-03 16:43:34 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/yogijosim/YogiJosimApplication.java
@@ -0,0 +1,13 @@
+package com.example.yogijosim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiJosimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogiJosimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yogijosim/YogiJosimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yogijosim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogiJosimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
