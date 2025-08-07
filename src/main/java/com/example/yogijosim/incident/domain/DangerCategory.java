17045a65e689603e10473857ae8be56dd16efe5a
fbehddn <tom0611@naver.com>
2025-08-07 23:36:20 +0900

    feat: create DangerCategory

---
+++ src/main/java/com/example/yogijosim/incident/domain/DangerCategory.java
@@ -0,0 +1,17 @@
+package com.example.yogijosim.incident.domain;
+
+public enum DangerCategory {
+	NATURAL_DISASTER("자연 재해"),
+	VIOLENT_CRIME("칼부림/총기"),
+	SUBWAY_FIRE("지하철 화재"),
+	PEST_OUTBREAK("러브버그/해충"),
+	SINKHOLE("싱크홀/도로 파손"),
+	ACCIDENT("사건/사고"),
+	ETC("기타");
+
+	private final String description;
+
+	DangerCategory(String description) {
+		this.description = description;
+	}
+}
