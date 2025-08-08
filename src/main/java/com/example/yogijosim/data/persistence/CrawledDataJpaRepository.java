c249cd351a1406003884fb1a2835cee1b0ff1cdb
fbehddn <tom0611@naver.com>
2025-08-08 15:28:37 +0900

    feature: find 3 not processed CrawledData

--- src/main/java/com/example/yogijosim/data/persistence/CrawledDataJpaRepository.java
+++ src/main/java/com/example/yogijosim/data/persistence/CrawledDataJpaRepository.java
@@ -3,5 +3,8 @@ package com.example.yogijosim.data.persistence;
 import com.example.yogijosim.data.domain.CrawledData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CrawledDataJpaRepository extends JpaRepository<CrawledData, Long> {
+	List<CrawledData> findTop3ByProcessedFalse();
 }
