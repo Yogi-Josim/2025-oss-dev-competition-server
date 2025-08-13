7e5698f3d8ac8a1caf355b428def42bea38c8102
fbehddn <tom0611@naver.com>
2025-08-13 20:28:15 +0900

    feat: classified by DangerCategory level

--- src/main/java/com/example/yogijosim/schedular/IncidentProcessingScheduler.java
+++ src/main/java/com/example/yogijosim/schedular/IncidentProcessingScheduler.java
@@ -6,9 +6,14 @@ import com.example.yogijosim.gpt.GptService;
 import com.example.yogijosim.gpt.IncidentAnalysisDto;
 import com.example.yogijosim.incident.domain.Incident;
 import com.example.yogijosim.incident.domain.IncidentRepository;
+import com.example.yogijosim.mail.MailService;
+import com.example.yogijosim.region.domain.Region;
+import com.example.yogijosim.subscription.domain.Subscription;
+import com.example.yogijosim.subscription.domain.SubscriptionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,7 +23,9 @@ import java.util.List;
 public class IncidentProcessingScheduler {
 	private final CrawledDataRepository crawledDataRepository;
 	private final IncidentRepository incidentRepository;
+	private final SubscriptionRepository subscriptionRepository;
 	private final GptService gptService;
+	private final MailService mailService;
 
 	@Scheduled(cron = "0 */5 * * * *")
 	@Transactional
@@ -31,12 +38,16 @@ public class IncidentProcessingScheduler {
 
 				Incident incident = Incident.from(analysisResult, data);
 				incidentRepository.save(incident);
+				if(incident.getDangerLevel()>=4){
+					sendEmergencyMail(incident);
+				}
 				data.completeProcessing();
 				Thread.sleep(2000);
 			}
 		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
 		}
+	}
 
 	}
 }
